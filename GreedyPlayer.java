cf7906ed40e1dba7e818ffce3cd6461796f0fded
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-08 17:16:00 +0200

    Add files via upload

--- GreedyPlayer.java
+++ GreedyPlayer.java
@@ -1,47 +1,50 @@
+package com.tema1.main;
+
 import java.util.ArrayList;
 
 public class GreedyPlayer extends BasePlayer {
 
-    public GreedyPlayer(int id){
+    public GreedyPlayer(final int id) {
         super(id);
     }
 
-    public void createSack(int round){
+    final void createSack(final int round) {
         /*System.out.println("GREEDY round " + round);
         System.out.println(this.hand);*/
         super.createSack(round);
-        if(round % 2 == 0){
-            if(sack.getItems().size() >= 8 || this.gold < 4){
+        if (round % 2 == 0) {
+            if (sack.getItems().size() >= Constants.MAX_ITEMS_IN_SACK
+                    || this.gold < Constants.MIN_GOLD_BAG) {
                 return;
             }
-            Goods max_profit_item = new Goods();
+            Goods maxProfitItem = new Goods();
             int ok = 0;
-            for(Goods good : hand){
-                if(good.getType() == GoodsType.Illegal && good.getProfit() > max_profit_item.getProfit()){
-                    max_profit_item = good;
+            for (Goods good : hand) {
+                if (good.getType() == GoodsType.Illegal
+                        && good.getProfit() > maxProfitItem.getProfit()) {
+                    maxProfitItem = good;
                     ok = 1;
                 }
             }
-            if(ok == 1) {
-                this.sack.addToSack(max_profit_item);
+            if (ok == 1) {
+                this.sack.addToSack(maxProfitItem);
             }
         }
     }
 
-    public ArrayList<Goods> verify(BasePlayer player, int initcoins) {
+    final ArrayList<Goods> verify(final BasePlayer player, final int initcoins) {
         int bribe = player.getSack().getBribe();
         ArrayList<Goods> items = new ArrayList<>();
-        if(bribe > 0){
+        if (bribe > 0) {
             this.gold += bribe;
             player.addToStall(player.getSack().getItems());
-        }
-        else{
+        } else {
             items = super.verify(player, initcoins);
         }
         return items;
     }
 
-    public void printScore(){
+    final void printScore() {
         System.out.println(this.id + " GREEDY " + this.score);
     }
 }
