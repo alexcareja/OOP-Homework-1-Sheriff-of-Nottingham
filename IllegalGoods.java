76c107182e911947e17b317743aaee99882aaee6
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-08 17:16:11 +0200

    Add files via upload

--- IllegalGoods.java
+++ IllegalGoods.java
@@ -1,15 +1,17 @@
+package com.tema1.main;
+
 import java.util.Map;
 
 
-public class IllegalGoods extends Goods {
+class IllegalGoods extends Goods {
 
     private final Map<Goods, Integer> illegalBonus;
 
 
-    public IllegalGoods(final int id, final int profit, final int penalty, final Map<Goods, Integer> illegalBonus) {
+    IllegalGoods(final int id, final int profit, final int penalty,
+                        final Map<Goods, Integer> illegalBonus) {
 
         super(id, GoodsType.Illegal, profit, penalty);
-
 
 
         this.illegalBonus = illegalBonus;
@@ -17,8 +19,7 @@ public class IllegalGoods extends Goods {
     }
 
 
-
-    public Map<Goods, Integer> getIllegalBonus() {
+    final Map<Goods, Integer> getIllegalBonus() {
 
         return illegalBonus;
 
