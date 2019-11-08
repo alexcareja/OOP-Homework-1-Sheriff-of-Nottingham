3d25b72338831df53f55ff37887afb34be7e3cc0
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-08 17:14:23 +0200

    Add files via upload

--- BasePlayer.java
+++ BasePlayer.java
@@ -1,84 +1,84 @@
+package com.tema1.main;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BasePlayer {
-    int id;
-    private static int s = 0;
-    int gold;
-    ArrayList<Goods> hand = new ArrayList<>();
-    Sack sack = new Sack();
-    private ArrayList<Goods> stall = new ArrayList<>();
-    int score;
+    protected final int id;
+    protected static int s = 0;
+    protected int gold;
+    protected ArrayList<Goods> hand = new ArrayList<>();
+    protected Sack sack = new Sack();
+    protected ArrayList<Goods> stall = new ArrayList<>();
+    protected int score;
 
-    public BasePlayer(){
-        this.gold = 80;
+    public BasePlayer() {
+        this.id = 0;
+        this.gold = Constants.INIT_GOLD;
         this.score = 0;
     }
 
-    public BasePlayer(int id){
+    public BasePlayer(final int id) {
         this.id = id;
-        this.gold = 80;
+        this.gold = Constants.INIT_GOLD;
         this.score = 0;
     }
 
-    public int getId(){
+    final int getId() {
         return this.id;
     }
 
-    public ArrayList<Goods> getStall(){
-        return this.stall;
-    }
-
-    public int getScore(){
+    final int getScore() {
         return this.score;
     }
 
-    public int getGold(){
+    final int getGold() {
         return this.gold;
     }
 
-    public Sack getSack(){
+    final Sack getSack() {
         return this.sack;
     }
 
-    public void drawHand(ArrayList<Goods> deck){
+    final void drawHand(final ArrayList<Goods> deck) {
         this.hand.clear();
-        this.hand.addAll(deck.subList(s, s+10));
-        s += 10;
+        this.hand.addAll(deck.subList(s, s + Constants.NO_CARDS_IN_HAND));
+        s += Constants.NO_CARDS_IN_HAND;
     }
 
-    public void printHand(){
-        System.out.println(hand);
-    }
-
-    public void createSack(int round){
+    /**
+     * Crearea sacului.
+     * @param round useful pentru greedy
+     */
+    void createSack(final int round) {
         //System.out.println("HAND" + this.hand);
         this.sack.clearSack();
         ArrayList<Goods> items = new ArrayList<>();
         int ok = 0;
-        for(Goods good : hand){
-            if(good.getType() == GoodsType.Legal){
+        for (Goods good : hand) {
+            if (good.getType() == GoodsType.Legal) {
                 ok = 1;
                 break;
             }
         }
-        if(ok == 0){    // Cazul in care nu are niciun obiect legal
-            if(this.gold < 4){  // Daca nu are suficiente monede pt a plati penalty creeaza un sac gol
-                return ;
+        if (ok == 0) {    // Cazul in care nu are niciun obiect legal
+            if (this.gold < Constants.MIN_GOLD_BAG) {
+                // Daca nu are suficient gold pt a plati,penalty face un sac gol
+                return;
             }
-            int max_profit = 0;
-            Goods max_profit_item = new Goods();
-            for(Goods good : hand){
-                if(good.getProfit() > max_profit){
-                    max_profit = good.getProfit();
-                    max_profit_item = good;
+            int maxProfit = 0;
+            Goods maxProfitItem = new Goods();
+            for (Goods good : hand) {
+                if (good.getProfit() > maxProfit) {
+                    maxProfit = good.getProfit();
+                    maxProfitItem = good;
                 }
             }
             Goods good = new Goods();
-            for(int i = 0; i < this.hand.size(); i++){
+            for (int i = 0; i < this.hand.size(); i++) {
                 good = this.hand.get(i);
-                if(max_profit_item.getId() == good.getId()){
+                if (maxProfitItem.getId() == good.getId()) {
                     this.hand.remove(i);
                     items.add(good);
                     break;
@@ -89,8 +89,8 @@ public class BasePlayer {
         }
 
         // Cazul in care exista cel putin un obiect legal
-        int max_fr = 0;
-        Goods max_fr_item = new Goods();
+        int maxFr = 0;
+        Goods maxFrItem = new Goods();
         Map<Goods, Integer> frequency = new HashMap<>();
 
         for (Goods good : hand) {
@@ -98,74 +98,82 @@ public class BasePlayer {
         }
 
         for (Goods key : frequency.keySet()) {
-            if(frequency.get(key) > max_fr && key.getType() == GoodsType.Legal){
-                max_fr = frequency.get(key);
-                max_fr_item = key;
+            if (frequency.get(key) > maxFr && key.getType() == GoodsType.Legal) {
+                maxFr = frequency.get(key);
+                maxFrItem = key;
                 continue;
             }
-            if(key.getType() == GoodsType.Legal &&
-                    ((frequency.get(key) == max_fr && key.getProfit() > max_fr_item.getProfit()) ||
-                    (frequency.get(key) == max_fr && key.getProfit() == max_fr_item.getProfit()
-                            && key.getId() > max_fr_item.getId()))){
-                max_fr_item = key;
+            if (key.getType() == GoodsType.Legal
+                    && ((frequency.get(key) == maxFr && key.getProfit() > maxFrItem.getProfit())
+                            || (frequency.get(key) == maxFr
+                                && key.getProfit() == maxFrItem.getProfit()
+                                    && key.getId() > maxFrItem.getId()))) {
+                maxFrItem = key;
             }
         }
-        for(Goods good : this.hand){
-            if(good.getId() == max_fr_item.getId()){
+        for (Goods good : this.hand) {
+            if (good.getId() == maxFrItem.getId()) {
                 items.add(good);
             }
-            if(items.size() == 8){
+            if (items.size() == Constants.MAX_ITEMS_IN_SACK) {
                 break;
             }
         }
-        this.sack.createSack(0, max_fr_item.getId(), items);
-//        System.out.println(this.sack);
+        this.sack.createSack(0, maxFrItem.getId(), items);
     }
 
-    public void updateGold(int value){
+    final void updateGold(final int value) {
         this.gold += value;
     }
 
-    public void addToStall(ArrayList<Goods> passed_goods){
-        this.stall.addAll(passed_goods);
+    final void addToStall(final ArrayList<Goods> passedGoods) {
+        this.stall.addAll(passedGoods);
     }
 
-    public ArrayList<Goods> verify(BasePlayer player, int initcoins){ // Returneaza cartile confiscate
-        Sack player_sack = player.getSack();
+    /**
+     * Crearea sacului.
+     * @param player playerul al carui sac e inspectat
+     * @param initcoins useful pentru bribed sa nu inspecteze inainte de a lua mita
+     * @return lista de carti confiscate
+     */
+    ArrayList<Goods> verify(final BasePlayer player, final int initcoins) {
+        Sack playerSack = player.getSack();
         ArrayList<Goods> confiscated = new ArrayList<>();
-        ArrayList<Goods> passed_goods = new ArrayList<>();
+        ArrayList<Goods> passedGoods = new ArrayList<>();
         // Returneaza mita daca exista
-        player.updateGold(player_sack.getBribe());
+        player.updateGold(playerSack.getBribe());
 
         // Seriful isi termina turul de serif daca nu are bani destui
-        if(this.gold < 16){
-            player.addToStall(player_sack.getItems());  // Playerul isi pune obiectele pe taraba
+        if (this.gold < Constants.MIN_GOLD_INSPECT) {
+            player.addToStall(playerSack.getItems());  // Playerul isi pune obiectele pe taraba
             return confiscated;
         }
 
         int penalty = 0;
-        Sack sack_copy = new Sack();
-        sack_copy.createSack(player_sack.getBribe(), player_sack.getItem_declared(), player_sack.getItems());
-        for(Goods good : sack_copy.getItems()) {
-            if (good.getId() != player_sack.getItem_declared() || good.getType() == GoodsType.Illegal) {
+        Sack sackCopy = new Sack();
+        sackCopy.createSack(playerSack.getBribe(), playerSack.getitemDeclared(),
+                playerSack.getItems());
+        for (Goods good : sackCopy.getItems()) {
+            if (good.getId() != playerSack.getitemDeclared()
+                    || good.getType() == GoodsType.Illegal) {
                 // Item ilegal sau nedeclarat, se confisca
                 // Calculeaza penalty (cat primeste seriful = cat pierde comerciantul)
                 penalty += good.getPenalty();
                 confiscated.add(good);
-                player_sack.removeFromSack(good);
+                playerSack.removeFromSack(good);
             }
         }
-        if(penalty == 0){   // Daca nu a mintit comerciantul, calculez cat datoreaza seriful
-            for(Goods good : player_sack.getItems()){
+        if (penalty == 0) {   // Daca nu a mintit comerciantul, calculez cat datoreaza seriful
+            for (Goods good : playerSack.getItems()) {
                 penalty -= good.getPenalty();
             }
         }
-        for(Goods good : player_sack.getItems()){
-            if(good.getId() == player_sack.getItem_declared()){
-                passed_goods.add(good);
+        for (Goods good : playerSack.getItems()) {
+            if (good.getId() == playerSack.getitemDeclared()) {
+                passedGoods.add(good);
             }
         }
-        player.addToStall(passed_goods);    // Playerul isi pune pe taraba obiectele cu care a trecut
+        player.addToStall(passedGoods);    // Playerul pune pe stall obiectele cu care a trecut
         // Se achita penalty
         this.gold += penalty;
         player.updateGold(-penalty);
@@ -175,43 +183,46 @@ public class BasePlayer {
         return confiscated;
     }
 
-    public void sellItems(){
-        for(Goods good : this.stall){   // Vinde toate itemele de pe taraba
+    final void sellItems() {
+        for (Goods good : this.stall) {   // Vinde toate itemele de pe taraba
             this.gold += good.getProfit();
         }
-        ArrayList<Goods> bonus_items = new ArrayList<>();
-        for(Goods good : this.stall) {   // Adauga bonusul adus de cartile ilegale la scor
+        ArrayList<Goods> bonusItems = new ArrayList<>();
+        for (Goods good : this.stall) {   // Adauga bonusul adus de cartile ilegale la scor
             if (good.getType() == GoodsType.Illegal) {
-                Map<Goods, Integer> bonus_map = ((IllegalGoods) good).getIllegalBonus();
-                for (Goods goodie : bonus_map.keySet()) {
-                    for (int i = bonus_map.get(goodie); i > 0; i--) {
-                        bonus_items.add(goodie);
+                Map<Goods, Integer> bonusMap = ((IllegalGoods) good).getIllegalBonus();
+                for (Goods goodie : bonusMap.keySet()) {
+                    for (int i = bonusMap.get(goodie); i > 0; i--) {
+                        bonusItems.add(goodie);
                         //this.stall.add(goodie);
                         this.score += goodie.getProfit();
                     }
                 }
             }
         }
-        this.stall.addAll(bonus_items);
+        this.stall.addAll(bonusItems);
         //System.out.println(this.gold);
         this.score += this.gold;
     }
 
-    public int getCardFrequency(int id){
+    final int getCardFrequency(final int goodId) {
         int counter = 0;
-        for(Goods goodie : this.stall){
-            if(goodie.getId() == id){
+        for (Goods goodie : this.stall) {
+            if (goodie.getId() == goodId) {
                 counter++;
             }
         }
         return counter;
     }
 
-    public void updateScore(int score){
-        this.score += score;
+    final void updateScore(final int newScore) {
+        this.score += newScore;
     }
 
-    public void printScore(){
+    /**
+     * Printarea scorului.
+     */
+    void printScore() {
         System.out.println(this.id + " BASIC " + this.score);
     }
 }
