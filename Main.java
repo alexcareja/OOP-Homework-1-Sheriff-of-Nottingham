2ca28318b22a7fe6faddc6edc8393c231b7f8f48
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-08 17:16:35 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+package com.tema1.main;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -6,31 +8,30 @@ import java.util.Map;
 
 public final class Main {
 
-
+    private Main() {
+    }
 
     public static void main(final String[] args) {
 
         GameInputLoader gameInputLoader = new GameInputLoader(args[0], args[1]);
 
         GameInput gameInput = gameInputLoader.load();
-        List<String> player_names = gameInput.getPlayerNames();
-        /*System.out.println(gameInput.getAssetIds());
-        System.out.println(player_names);
-        System.out.println(gameInput.getRounds());*/
+        List<String> playerNames = gameInput.getPlayerNames();
+
 
         // Crearea deck-ului cu bunuri
         ArrayList<Goods> deck = new ArrayList<>();
         GoodsFactory factory = GoodsFactory.getInstance();
-        for(int item : gameInput.getAssetIds()){
+        for (int item : gameInput.getAssetIds()) {
             deck.add(factory.getGoodsById(item));
         }
 
         // Declararea jucatorilor
-        ArrayList<BasePlayer> players = new ArrayList<BasePlayer>();
+        ArrayList<BasePlayer> players = new ArrayList<>();
 
         int id = 0;
-        for(String s: gameInput.getPlayerNames()){
-            switch (s){
+        for (String s : gameInput.getPlayerNames()) {
+            switch (s) {
                 case "basic":
                     players.add(new BasePlayer(id));
                     break;
@@ -40,84 +41,76 @@ public final class Main {
                 case "greedy":
                     players.add(new GreedyPlayer(id));
                     break;
+                default:
+                    break;
             }
             id++;
         }
 
-        int no_players = player_names.size();
-        int no_rounds = gameInput.getRounds();
-        if(no_rounds > 7){
-            no_rounds = 7;
+        int noPlayerss = playerNames.size();
+        Utils.getInstance(noPlayerss);  // Setez noPlayerss in Utils
+        int noRounds = gameInput.getRounds();
+        if (noRounds > Constants.MAX_ROUNDS) {
+            noRounds = Constants.MAX_ROUNDS;
         }
-        BasePlayer p = new BasePlayer();
-        BasePlayer sheriff = new BasePlayer();
+        BasePlayer p;
+        BasePlayer sheriff;
         int initgold;
-        for(int round = 1; round <= no_rounds; round++){
-            System.out.println("RUNDA " + round);
-            for(int sub_round = 1; sub_round <= no_players; sub_round++){
-                System.out.println("SUBRUNDA " + sub_round);
-                sheriff = players.get(sub_round - 1);
+        for (int round = 1; round <= noRounds; round++) {
+            for (int subRound = 1; subRound <= noPlayerss; subRound++) {
+                sheriff = players.get(subRound - 1);
                 initgold = sheriff.getGold();
-                System.out.println("SHERIFF GOLD = " + sheriff.getGold());
-                for(int i = 1; i <= no_players; i++){
-                    if(i == sub_round){
+                for (int i = 1; i <= noPlayerss; i++) {
+                    if (i == subRound) {
                         continue;
                     }
                     p = players.get(i - 1);
                     p.drawHand(deck);   // Ia 10 carti
-                    System.out.println("Banii lui " + i + " inainte de inspectie = " + p.getGold());
-                    System.out.println("Cartile lui " + i);
-                    p.printHand();
                     p.createSack(round);     // Creeaza sacul
-                    System.out.println("Sacul lui " + i);
-                    System.out.println(p.getSack());
                     // Verificarea sacului si adaugarea cartilor confiscate in deck
                     deck.addAll(sheriff.verify(p, initgold));
-                    System.out.println("Banii lui " + i + " dupa de inspectie = " + p.getGold());
-                    System.out.println("Taraba lui" + i);
-                    System.out.println(p.getStall());
-                    /*System.out.println("Dupa inspectie sheriful are "+ sheriff.getGold());
-                    System.out.println("Dupa inspectie comerciantul are " +p.getGold());*/
                 }
             }
         }
-        for(BasePlayer plr : players){
+        for (BasePlayer plr : players) {
             plr.sellItems();
         }
-        Goods good = new Goods();
-        int max_freq, second_max, freq, bonus;
-        for(int i = 0; i <= 9; i++){    // Crearea map-ului
-            max_freq = second_max = 0;
+        Goods good;
+        int maxFreq, secondMax, freq, bonus;
+        for (int i = 0; i <= Constants.HIGHEST_LEGAL_ID; i++) {    // Crearea map-ului
+            maxFreq = 0;
+            secondMax = 0;
             good = factory.getGoodsById(i);
-            bonus = ((LegalGoods)good).getKingBonus();
-            Map<BasePlayer, Integer> bonus_map = new HashMap<>();
-            for(int j = 0; j < no_players; j++){
+            bonus = ((LegalGoods) good).getKingBonus();
+            Map<BasePlayer, Integer> bonusMap = new HashMap<>();
+            for (int j = 0; j < noPlayerss; j++) {
                 p = players.get(j);
                 freq = p.getCardFrequency(good.getId());
-                bonus_map.put(p, freq);
-                if(freq > max_freq){
-                    second_max = max_freq;
-                    max_freq = freq;
+                bonusMap.put(p, freq);
+                if (freq > maxFreq) {
+                    secondMax = maxFreq;
+                    maxFreq = freq;
                     continue;
                 }
-                if(freq > second_max){
-                    second_max = freq;
+                if (freq > secondMax) {
+                    secondMax = freq;
                 }
 
             }
-            for(BasePlayer plr : players){
-                if(bonus_map.get(plr) == max_freq && max_freq != 0){
+            for (BasePlayer plr : players) {
+                if (bonusMap.get(plr) == maxFreq && maxFreq != 0) {
+                    // king bonus -> queen bonus -> break
                     plr.updateScore(bonus);
-                    if(bonus == ((LegalGoods)good).getQueenBonus()){
+                    if (bonus == ((LegalGoods) good).getQueenBonus()) {
                         bonus = 0;
                         break;
                     }
-                    bonus = ((LegalGoods)good).getQueenBonus();    // king bonus -> queen bonus -> break
+                    bonus = ((LegalGoods) good).getQueenBonus();
                 }
             }
-            if(bonus != 0){
-                for(BasePlayer plr : players){
-                    if(bonus_map.get(plr) == second_max && second_max != 0){
+            if (bonus != 0) {
+                for (BasePlayer plr : players) {
+                    if (bonusMap.get(plr) == secondMax && secondMax != 0) {
                         plr.updateScore(bonus);
                         break;
                     }
@@ -125,7 +118,7 @@ public final class Main {
             }
         }
         players.sort((BasePlayer p1, BasePlayer p2) -> p2.getScore() - p1.getScore());
-        for(BasePlayer plr: players){
+        for (BasePlayer plr : players) {
             plr.printScore();
         }
     }
